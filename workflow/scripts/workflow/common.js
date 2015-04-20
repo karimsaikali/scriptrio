8cc17a3fd18ebb70661f97cf512ee6659f29d83c
Karim Saikali <karim.saikali@elementn.com>
2015-04-20 18:04:55 +0300

    First commit of workflow framework

---
+++ workflow/scripts/workflow/common.js
@@ -0,0 +1,13 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var eventQueuePrefix = "EventQueue_";  
+var processDefinitionPrefix = "ProcessDef_";
+var processPrefix = "Process_";
+var activityPrefix = "Activity_";
+var NOT_RUNNING = "not_running"; 
+var RUNNING = "running";
+var DONE = "done";
+var CANCELLED = "cancelled";   							
---
+++ workflow/scripts/workflow/humanActivityDefinition.js
@@ -0,0 +1,17 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var activityModuleDefinition = require("workflow/activityDefinition");
+
+function HumanActivityDefinition() {
+  
+}
+
+HumanActivityDefinition.prototype = new activityModuleDefinition.ActivityDefinition()
+HumanActivityDefinition.prototype.constructor = HumanActivityDefinition;
+
+HumanActivityDefinition.prototype.execute = function() {
+  
+}   							
